ce6c206ac056f7ce39c8a2cbeaf1e593749bbf51
ariaandika <andikaakun17@gmail.com>
2024-11-01 21:31:27 +0700

    add command component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
+<script>
+  import Command from "./Command.svelte";
+</script>
 
-<section class="container w-full h-screen p-16">
-  <h1 class="w-full text-center text-4xl font-bold text-white">
+
+<section class="container mx-auto w-full h-screen p-16">
+  <h1 class="my-4 w-full text-center text-4xl font-bold text-white">
     Tauri Dev
   </h1>
+  <Command/>
 </section>
 
---
+++ src/routes/Command.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+
+  let promise = $state(Promise.resolve("None"));
+  let message = $state("Send message to rust");
+
+  function send() {
+    promise = invoke("custom_command", { message })
+  }
+
+</script>
+
+
+<section class="relative p-8 w-full rounded-md bg-gray-800">
+  <div class="mb-4">
+    <kbd
+      class="py-1 px-2 bg-gray-900 text-gray-400 rounded-md"
+    >
+        src/routes/Command.svelte
+    </kbd>
+  </div>
+
+  <h1 class="mb-4 text-3xl font-bold"># Command</h1>
+
+  <div class="mb-4">Call rust function from js</div>
+
+  <label>
+    <input bind:value={message} class="px-3 py-2 mb-4 bg-gray-900 text-gray-400 rounded-md">
+    <button
+      onclick={send}
+      class="px-3 py-2 font-bold bg-gray-900 rounded-md hover:bg-gray-950 transition-colors"
+    >
+      Send
+    </button>
+  </label>
+
+  <div class="mb-4">
+    {#await promise}
+      Loading...
+    {:then msg}
+      From Rust: {msg}
+    {:catch err}
+      Error: {err?.message ?? "Fatal Error"}
+    {/await}
+  </div>
+</section>
+
+
+
